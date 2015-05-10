0a7f43866e91b2a2f6132872b6a5cf4de6d82e53
山村 達彦 <ruitomo.ex@gmail.com>
2015-05-10 21:43:20 +0900

    UIアニメーションを追加

---
+++ Assets/UI/SpriteColor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteColor : MonoBehaviour {
+
+	public Color color{ get; set;}
+
+	public enum ColorType{
+		Red,
+		Yellow
+	}
+
+	public ColorType type{get; set;}
+
+	public void UpdateColor()
+	{
+		GetComponent<UnityEngine.UI.Image>().color = color;
+	}
+}
